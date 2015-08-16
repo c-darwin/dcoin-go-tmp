2bece3da48f4986635d4ef85891cf52ff7f6fbbf
Darwin <darwin84@yandex.ru>
2015-08-16 18:22:19 +0300

    upd

--- dcoinandroid.go
+++ dcoinandroid.go
@@ -7,7 +7,7 @@ import (
 
 	"image"
 	"log"
-	"math"
+//"math"
 	"time"
 
 	_ "image/png"
@@ -16,12 +16,12 @@ import (
 	"golang.org/x/mobile/asset"
 	"golang.org/x/mobile/event/config"
 	"golang.org/x/mobile/event/paint"
-	"golang.org/x/mobile/exp/app/debug"
+//"golang.org/x/mobile/exp/app/debug"
 	"golang.org/x/mobile/exp/f32"
 	"golang.org/x/mobile/exp/sprite"
 	"golang.org/x/mobile/exp/sprite/clock"
 	"golang.org/x/mobile/exp/sprite/glsprite"
-	"golang.org/x/mobile/gl"
+//"golang.org/x/mobile/gl"
 	"fmt"
 )
 
@@ -40,12 +40,15 @@ var (
 	startTime = time.Now()
 	eng       = glsprite.Engine()
 	scene     *sprite.Node
+	cfg config.Event
 )
 
 func main() {
 
 	dir:= C.GoString(C.JGetTmpDir2());
 	fmt.Println("dir111()>::", dir)
+	fmt.Println("dir122()>::", C.GoString(C.getenv(C.CString("FILESDIR"))))
+	fmt.Println("dir122()>::", C.GoString(C.getenv(C.CString("TMPDIR"))))
 
 	//go func(dir string) {
 	//  dcoin.Start(dir)
@@ -53,13 +56,14 @@ func main() {
 	go dcoin.Start(dir)
 
 	app.Main(func(a app.App) {
-		var c config.Event
+
 		for e := range a.Events() {
+			fmt.Println("e:", e)
 			switch e := app.Filter(e).(type) {
 				case config.Event:
-				c = e
+				cfg = e
 				case paint.Event:
-				onPaint(c)
+				onPaint(cfg)
 				a.EndPaint(e)
 			}
 		}
@@ -67,14 +71,9 @@ func main() {
 }
 
 func onPaint(c config.Event) {
-	if scene == nil {
-		loadScene()
-	}
-	gl.ClearColor(1, 1, 1, 1)
-	gl.Clear(gl.COLOR_BUFFER_BIT)
+	loadScene()
 	now := clock.Time(time.Since(startTime) * 60 / time.Second)
 	eng.Render(scene, now, c)
-	debug.DrawFPS(c)
 }
 
 func newNode() *sprite.Node {
@@ -93,43 +92,19 @@ func loadScene() {
 		{0, 1, 0},
 	})
 
-	var n *sprite.Node
+	//var n *sprite.Node
+	new_w := float32(cfg.WidthPt)
+	new_h := new_w*1.77
+	if float32(cfg.WidthPt)/float32(cfg.HeightPt) > 1 {
+		new_w = float32(cfg.WidthPt)
+		new_h = float32(cfg.WidthPt)*0.5625
 
-	n = newNode()
-	eng.SetSubTex(n, texs[texBooks])
+	}
+	n := newNode()
+	eng.SetSubTex(n, texs)
 	eng.SetTransform(n, f32.Affine{
-		{36, 0, 0},
-		{0, 36, 0},
-	})
-
-	n = newNode()
-	eng.SetSubTex(n, texs[texFire])
-	eng.SetTransform(n, f32.Affine{
-		{72, 0, 144},
-		{0, 72, 144},
-	})
-
-	n = newNode()
-	n.Arranger = arrangerFunc(func(eng sprite.Engine, n *sprite.Node, t clock.Time) {
-		// TODO: use a tweening library instead of manually arranging.
-		t0 := uint32(t) % 120
-		if t0 < 60 {
-			eng.SetSubTex(n, texs[texGopherR])
-		} else {
-			eng.SetSubTex(n, texs[texGopherL])
-		}
-
-		u := float32(t0) / 120
-		u = (1 - f32.Cos(u*2*math.Pi)) / 2
-
-		tx := 18 + u*48
-		ty := 36 + u*108
-		sx := 36 + u*36
-		sy := 36 + u*36
-		eng.SetTransform(n, f32.Affine{
-			{sx, 0, tx},
-			{0, sy, ty},
-		})
+		{new_w, 0, 0},
+		{0, new_h, 0},
 	})
 }
 
@@ -140,8 +115,17 @@ const (
 	texGopherL
 )
 
-func loadTextures() []sprite.SubTex {
-	a, err := asset.Open("logo-big.png")
+func loadTextures() sprite.SubTex {
+	imgPath := "mobile.png"
+	w := 1080
+	h := 1920
+	if float32(cfg.WidthPt)/float32(cfg.HeightPt) > 1 {
+		imgPath = "mobile-landscape.png"
+		w = 1920
+		h = 1080
+	}
+	a, err := asset.Open(imgPath)
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -156,12 +140,7 @@ func loadTextures() []sprite.SubTex {
 		log.Fatal(err)
 	}
 
-	return []sprite.SubTex{
-		texBooks:   sprite.SubTex{t, image.Rect(4, 71, 132, 182)},
-		texFire:    sprite.SubTex{t, image.Rect(330, 56, 440, 155)},
-		texGopherR: sprite.SubTex{t, image.Rect(152, 10, 152+140, 10+90)},
-		texGopherL: sprite.SubTex{t, image.Rect(162, 120, 162+140, 120+90)},
-	}
+	return sprite.SubTex{t, image.Rect(0, 0, w, h)}
 }
 
 type arrangerFunc func(e sprite.Engine, n *sprite.Node, t clock.Time)
