ae2957b51f043d78f69835ab9eedc3bd9e9bf60d
Your Name <you@example.com>
2015-08-30 10:38:54 +0300

    upd

--- packages/utils/sql.go
+++ packages/utils/sql.go
@@ -1840,9 +1840,11 @@ func (db *DCDB) GetMyUsersIds(checkCommission, checkNodeKey bool) ([]int64, erro
 					return usersIds, err
 				}
 				if publicKey != nodePublicKey {
+					log.Debug("usersIds: %v", usersIds)
 					log.Debug("DelUserIdFromArray publicKey != nodePublicKey (%x != %x) %v", publicKey, nodePublicKey, uid)
 					DelUserIdFromArray(&usersIds, StrToInt64(uid));
 				}
+				log.Debug("usersIds: %v", usersIds)
 			}
 		}
 	}
