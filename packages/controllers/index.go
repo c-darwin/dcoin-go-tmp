5ee17ac7856781d55de93fb031f36369d823d53d
Your Name <you@example.com>
2015-10-02 00:27:56 +0300

    upd

--- packages/controllers/index.go
+++ packages/controllers/index.go
@@ -105,8 +105,8 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		Lang: globalLangReadOnly[lang],
 		Key: key,
 		SetLang: setLang,
-		IOS: utils.IOS(),
-		Mobile: utils.Mobile()})
+		IOS: true,
+		Mobile: true})
 	if err != nil {
 		log.Error("%v", err)
 	}
