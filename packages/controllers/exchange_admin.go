6280c379a046917fe524e27621971eca815d7145
Your Name <you@example.com>
2015-11-04 17:14:42 +0300

    upd

--- packages/controllers/exchange_admin.go
+++ packages/controllers/exchange_admin.go
@@ -10,6 +10,8 @@ type ExchangeAdminPage struct {
 	UserId       int64
 	Lang         map[string]string
 	Withdraw []map[string]string
+	Lock int64
+	Users []map[string]string
 }
 
 func (c *Controller) ExchangeAdmin() (string, error) {
@@ -19,36 +21,51 @@ func (c *Controller) ExchangeAdmin() (string, error) {
 	}
 
 	log.Debug("c.Parameters", c.Parameters)
+	withdrawId := utils.StrToInt64(c.Parameters["withdraw_id"])
+	if withdrawId > 0 {
+		err := c.ExecSql(`UPDATE e_withdraw SET close_time = ? WHERE id = ?`, utils.Time(), withdrawId)
+		if err!=nil {
+			return "", utils.ErrInfo(err)
+		}
+	}
+
+	lock, err := c.Single(`SELECT time FROM e_reduction_lock`).Int64()
+	if len(c.Parameters["change_reduction_lock"]) > 0 {
+		if lock > 0 {
+			err := c.ExecSql(`DELETE FROM e_reduction_lock`)
+			if err!=nil {
+				return "", utils.ErrInfo(err)
+			}
+		} else {
+			err := c.ExecSql(`INSERT INTO e_reduction_lock (time) VALUES (?)`, utils.Time())
+			if err!=nil {
+				return "", utils.ErrInfo(err)
+			}
+		}
+
+	}
 
 	withdraw, err := c.GetAll(`SELECT e_withdraw.id, open_time, close_time, e_users.user_id, currency_id, account, amount,  wd_amount, method, email
     		FROM e_withdraw
     		LEFT JOIN e_users on e_users.id = e_withdraw.user_id
-   			ORDER BY open_time DESC`, 100)
-	/*rows, err := c.Query(c.FormatQuery(`
-			SELECT e_withdraw.id, open_time, close_time, user_id, currency_id, account, amount,  wd_amount, method, email
-    		FROM e_withdraw
-    		LEFT JOIN e_users on e_users.id = e_withdraw.user_id
-   			ORDER BY open_time DESC
-			`))
-	if err != nil {
+   			ORDER BY id DESC`, 100)
+	if err!=nil {
 		return "", utils.ErrInfo(err)
 	}
-	defer rows.Close()
-	for rows.Next() {
-		var id, open_time, close_time, user_id, currency_id int64
-		var account, method, email string
-		var amount, wd_amount float64
-		err = rows.Scan(&id, &open_time, &close_time, &user_id, &currency_id, &account, &amount,  &wd_amount, &method, &email)
-		if err != nil {
-			return "", utils.ErrInfo(err)
-		}
-	}*/
 
+	users, err := c.GetAll(`SELECT id, email, ip, lock, user_id
+    		FROM e_users
+   			ORDER BY id DESC`, 100)
+	if err!=nil {
+		return "", utils.ErrInfo(err)
+	}
 
 	TemplateStr, err := makeTemplate("exchange_admin", "exchangeAdmin", &ExchangeAdminPage{
 		Alert:        c.Alert,
 		Lang:         c.Lang,
 		Withdraw: withdraw,
+		Lock: lock,
+		Users: users,
 		UserId:       c.SessUserId})
 	if err != nil {
 		return "", utils.ErrInfo(err)
