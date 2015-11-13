4a00e782f46d6882501ada8d8b9aad94d988ecb8
Your Name <you@example.com>
2015-11-13 17:10:25 +0300

    upd

--- static/js/index.js
+++ static/js/index.js
@@ -85,13 +85,10 @@ function login_ok (result) {
         $('.modal-backdrop').remove();
         $('.modal-backdrop').css('display', 'none');
 
-        console.log("$('#exchangeTemplate').val()", $('#exchangeTemplate').val());
-
         if ($('#exchangeTemplate').val() == "1") {
 
             $('#SigninKeyModal').modal('hide');
-            console.log("window.location.href", $('#Ehost').val());
-            //window.location.href = $('#Ehost').val();
+            window.location.href = $('#EHost').val();
 
         } else if (typeof(get_key_and_sign)==='undefined' || get_key_and_sign=='null') {
 
