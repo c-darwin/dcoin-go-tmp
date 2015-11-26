ae41f7bdccbd9636f393d4db1224b4596f4f3273
Your Name <you@example.com>
2015-11-26 20:50:25 +0300

    upd

--- make_freebsd_zip/make.sh
+++ make_freebsd_zip/make.sh
@@ -1,5 +1,6 @@
 go get -u github.com/c-darwin/dcoin-go-tmp
-setenv GOARCH amd64 && setenv CGO_ENABLED 1 && go build -o dcoin64
-setenv GOARCH 386 && setenv CGO_ENABLED 1 && go build -o dcoin32
+export CGO_ENABLED=1
+export GOARCH=amd64 && go build -o dcoin64
+export GOARCH=386 && go build -o dcoin32
 zip dcoin_freebsd64.zip dcoin64
 zip dcoin_freebsd32.zip dcoin32
